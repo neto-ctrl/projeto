e923d236b194e209ac66efd67b1d23ce0a1fe1b6
neto-ctrl <158202486+neto-ctrl@users.noreply.github.com>
2024-07-22 13:30:40 -0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,19 +5,9 @@ const Home: NextPage = () => {
   return (
     <div>
       <Head>
-        <title>Minha Página Next.js com TypeScript</title>
-        <meta name="description" content="Criando uma página Next.js com TypeScript" />
-        <link rel="icon" href="/favicon.ico" />
+        <title></title>
+        
       </Head>
-
-      <main>
-        <h1>Bem-vindo à minha página Next.js com TypeScript</h1>
-        <p>Esta é uma página simples criada com Next.js e TypeScript.</p>
-      </main>
-
-      <footer>
-        <p>Feito com ❤️ por [Seu Nome]</p>
-      </footer>
     </div>
   );
 };
